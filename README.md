8d0921f7528880aed03e1458a368fb7597bfb4db
patmood <patrick.n.moody@gmail.com>
2014-10-26 14:02:15 -0700

    added gatekeeper

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ Time machine for Github files
 
 - Authentication
   - Setup simple express server with https://github.com/prose/gatekeeper
-  -
+  - login https://github.com/login/oauth/authorize?client_id=3dbe9b15a57c1f2ae62d&scope=repo
+  - get code from above and send to http://localhost:9999/authenticate/:code
+  - Use that token in all the requests
+  - force login if no token exists
+  - save token in cookie, destroy cookie on logout
+
 - Prevent dups!!! Collection#add is not being hit due to cache and/or 304 not modified response...
 - Render list of commits in a timeline at the top of the page https://github.com/almende/vis/blob/master/examples/timeline/01_basic.html
 - Chrome app to link directly from GH
@@ -16,4 +21,3 @@ Time machine for Github files
 ## NOTES
 
 - The github 'sha' can also be a branch
-
---
+++ server.js
@@ -0,0 +1,65 @@
+var url     = require('url'),
+    http    = require('http'),
+    https   = require('https'),
+    fs      = require('fs'),
+    qs      = require('querystring'),
+    dotenv  = require('dotenv')
+
+dotenv.load()
+
+var express = require('express'),
+    app     = express();
+
+function authenticate(code, next) {
+  var data = qs.stringify({
+    client_id: process.env.OAUTH_CLIENT_ID,
+    client_secret: process.env.OAUTH_CLIENT_SECRET,
+    code: code
+  });
+
+  var reqOptions = {
+    host: process.env.OAUTH_HOST,
+    port: process.env.OAUTH_PORT,
+    path: process.env.OAUTH_PATH,
+    method: process.env.OAUTH_METHOD,
+    headers: { 'content-length': data.length }
+  };
+
+  var body = "";
+  var req = https.request(reqOptions, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) { body += chunk; });
+    res.on('end', function() {
+      next(null, qs.parse(body).access_token);
+    });
+  });
+
+  req.write(data);
+  req.end();
+  req.on('error', function(e) { next(e.message); });
+}
+
+
+// Convenience for allowing CORS on routes - GET only
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app.get('/authenticate/:code', function(req, res) {
+  console.log('authenticating code:' + req.params.code);
+  authenticate(req.params.code, function(err, token) {
+    var result = err || !token ? {"error": "bad_code"} : { "token": token };
+    console.log(result);
+    res.json(result);
+  });
+});
+
+var port = process.env.PORT || 9999;
+
+app.listen(port, null, function (err) {
+  console.log('Gatekeeper, at your service: http://localhost:' + port);
+});
