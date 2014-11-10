da54da9bc5cecb6bffebceab2682ffee3b4e9afb
patmood <patrick.n.moody@gmail.com>
2014-11-09 21:18:58 -0800

    more reliable button insertion

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Git Time Machine
 Time machine for Github files
 
 ## TODO
-- Chrome app inserts button on every page (not just first load)
+- Match github's url structure exactly so it can link to 'mydomain.com' + window.location.pathname
 - Freeze timeline and nav header
 - Clicking timeline commit goes to that commit
 - Select the current commit on the timeline when using next/prev buttons
--- browser_extensions/chrome/time_machine_button.js
+++ browser_extensions/chrome/time_machine_button.js
@@ -1,4 +1,36 @@
-(function() {
+var timeout, addButton
+
+function listener() {
+  // Return if button already inserted
+  if (document.getElementById('time-machine')) return;
+  addButton()
+}
+
+
+document.addEventListener("DOMSubtreeModified", function() {
+  if(timeout) clearTimeout(timeout)
+  timeout = setTimeout(listener, 500)
+}, false)
+
+var addButton = function() {
+  var buttonGroup = document.querySelectorAll('.file-box .actions .button-group')
+
+  // Return if no buttons on page
+  if (buttonGroup.length === 0) return;
+
+  // Get details from url
+  var pattern = /^\/(\w+)\/(\w+)\/blob\/(\w+)\/(.+)$/i
+    , reg = new RegExp(pattern)
+    , match = window.location.pathname.match(reg)
+
+  // Return if no url match
+  if (!match) return;
+
+  var username = match[1]
+    , repo = match[2]
+    , sha = match[3]
+    , path = match[4]
+
   // Create link
   var link = document.createElement('a')
   var t = document.createTextNode('Time Machine')
@@ -11,16 +43,8 @@
   var imgURL = chrome.extension.getURL("galaxy_button.png")
   link.style.backgroundImage="url('" + imgURL + "')"
 
-  // Get url
-  var pattern = /^\/(\w+)\/(\w+)\/blob\/(\w+)\/(.+)$/i
-    , reg = new RegExp(pattern)
-    , match = window.location.pathname.match(reg)
-
-  var username = match[1]
-    , repo = match[2]
-    , sha = match[3]
-    , path = match[4]
-
+  // Link to Time Machine page
+  // TODO: Match github's url structure exactly so it can link to 'mydomain.com' + window.location.pathname
   link.href = 'http://localhost:9999/repos/'
              + username
              + '/'
@@ -31,6 +55,6 @@
              + path
 
   // Add to page
-  document.querySelectorAll('.file-box .actions .button-group')[0].appendChild(link)
+  buttonGroup[0].appendChild(link)
 
-})()
+}
