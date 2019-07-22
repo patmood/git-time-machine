456751284b1c6847a5b5dbe47fa1ce0d8bdfb597
patmood <patrick.n.moody@gmail.com>
2019-07-22 06:57:20 -0700

    better blob loading

--- src/Blob.js
+++ src/Blob.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
-import { Loading } from './Loading'
+import cn from 'classnames'
 import { Error } from './Error'
+
+import css from './Blob.module.css'
 
 const textQuery = gql`
   query File($repo: String!, $owner: String!, $expression: String!) {
@@ -19,16 +21,19 @@ const textQuery = gql`
 const Blob = React.memo(function Blob(props) {
   const { owner, repo, gitRef, path } = props
   const expression = `${gitRef}:${path}`
-
+  const [text, setText] = useState('')
   return (
     <div>
       <h1>Blob</h1>
       <Query query={textQuery} variables={{ owner, repo, expression }}>
         {({ loading, error, data }) => {
-          if (loading) return <Loading />
           if (error) return <Error error={error} />
 
-          return <pre>{data.repository.object.text}</pre>
+          if (!loading) {
+            setText(data.repository.object.text)
+          }
+
+          return <pre className={cn({ [css.loading]: loading })}>{text}</pre>
         }}
       </Query>
     </div>
